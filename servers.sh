5c4748ff1b3f68d3dbabf8f65c4eb74e86f8e2fa
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2025-02-13 13:13:19 +0000

    Update servers.sh

--- servers.sh
+++ servers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose -f "/controlpanel_docker/servers/docker-compose.yml" down
-docker-compose -f "/controlpanel_docker/servers/docker-compose.yml" up -d
+docker-compose -f "controlpanel_docker/servers/docker-compose.yml" down
+docker-compose -f "controlpanel_docker/servers/docker-compose.yml" up -d
