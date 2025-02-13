cb3905fb312bd745e913d709e88c09519142db77
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2025-02-13 14:50:28 +0000

    files_update

--- _NotInUse_update_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p www
-wget -O ./www/index.php https://raw.githubusercontent.com/storjdashboard/storjdashboard/main/public/index.php
-wget -O ./www/daily.php https://raw.githubusercontent.com/storjdashboard/storjdashboard/main/public/daily.php
-wget -O ./www/pay.php https://raw.githubusercontent.com/storjdashboard/storjdashboard/main/public/pay.php
-wget -O ./www/audit.php https://raw.githubusercontent.com/storjdashboard/storjdashboard/main/public/audit.php
